3fe86cf34bab54c3f32d2525dc40fc41e8ef5028
Aryan <aryansharma11223@gmail.com>
2021-03-15 11:06:55 +0530

    After a long time

---
+++ resource/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    $('.js-section-features').waypoint(function(direction){
+
+        if(direction == "down")
+        {
+            $('nav').addClass('sticky');
+        }
+        else
+        {
+            $('nav').removeClass('sticky');
+
+        }
+        
+    }, {
+        offset: '60px;'
+    });
+
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({
+            scrollTop :$('.js--section-plans').offset().top
+        }, 1000);
+    });
+
+
+});
+
+
+
+
+// var waypoints = $('#handler-first').waypoint(function(direction) {
+//     notify(this.element.id + ' hit 25% from top of window') 
+//   }, {
+//     offset: '25%'
+//   })
